80ae034b63d2a520e1c02bb9ad05b7fccab5faf7
bojko108 <bojko108@gmail.com>
2019-08-09 15:36:57 +0300

    change filter property names

--- src/ol/daemon/filters/index.js
+++ src/ol/daemon/filters/index.js
@@ -1,7 +1,7 @@
 /**
  * @typedef FilterType
- * @property {!String} attributeName
- * @property {!Object} validValue
+ * @property {!String} field
+ * @property {!Object} value
  * @property {EnumOperators} [operator='equal']
  */
 
@@ -11,84 +11,84 @@
  */
 export const EnumOperators = Object.freeze({
   /**
-   * For **Number** - `attributeName` is in range of `validValue`
+   * For **Number** - `field` is in range of `value`
    * @example
    * COUNT BETWEEN [1,100]
    * // select features with COUNT in range 1, 100
    */
   BETWEEN: 'between',
   /**
-   * For **String** - `attributeName` contains `validValue`
+   * For **String** - `field` contains `value`
    * @example
    * NAME CONTAIN 'BG'
    * // select features with NAME containing 'BG'
    */
   CONTAIN: 'contain',
   /**
-   * For **Number** - `attributeName` is equal to `validValue`
+   * For **Number** - `field` is equal to `value`
    * @example
    * POWER EQUAL 20
    * // select features with POWER = 20kV
    */
   EQUAL: 'equal',
   /**
-   * For **Number** - `attributeName` is greater than `validValue`
+   * For **Number** - `field` is greater than `value`
    * @example
    * POWER GREATER_THAN 20
    * // select features with POWER > 20kV
    */
   GREATER_THAN: 'greaterThan',
   /**
-   * For **Number** - `attributeName` is greater than or equal to `validValue`
+   * For **Number** - `field` is greater than or equal to `value`
    * @example
    * POWER GREATER_THAN_OR_EQUAL 20
    * // select features with POWER >= 20kV
    */
   GREATER_THAN_OR_EQUAL: 'greaterThanOrEqual',
   /**
-   * For **String** - `attributeName` is equal to `validValue`
+   * For **String** - `field` is equal to `value`
    * @example
    * NAME LIKE 'Full Match'
    * // select features with NAME = 'Full Match'
    */
   LIKE: 'like',
   /**
-   * For **Number** - `attributeName` is less than `validValue`
+   * For **Number** - `field` is less than `value`
    * @example
    * POWER LESS_THAN 20
    * // select features with POWER < 20kV
    */
   LESS_THAN: 'lessThan',
   /**
-   * For **Number** - `attributeName` is less than or equal `validValue`
+   * For **Number** - `field` is less than or equal `value`
    * @example
    * POWER LESS_THAN_OR_EQUAL 20
    * // select features with POWER <= 20kV
    */
   LESS_THAN_OR_EQUAL: 'lessThanOrEqual',
   /**
-   * For **String** - `attributeName` is not equal to `validValue`
+   * For **String** - `field` is not equal to `value`
    * @example
    * NAME NOT_LIKE 'Full Match'
    * // select features with NAME != 'Full Match'
    */
   NOT_LIKE: 'notLike',
   /**
-   * For **Number** - `attributeName` is not equal to `validValue`
+   * For **Number** - `field` is not equal to `value`
    * @example
    * POWER NOT_EQUAL 20
    * // select features with POWER <> 20kV
    */
   NOT_EQUAL: 'notEqual',
   /**
-   * For **Number, String** - `attributeName` is in `validValue`
+   * For **Number, String** - `field` is in `value`
    * @example
    * POWER IN (10, 20)
    * // select features with POWER = 10 and POWER = 20
    */
   IN: 'in',
   /**
-   * For **Number, String** - `attributeName` is not in `validValue`
+   * For **Number, String** - `field` is not in `value`
    * @example
    * POWER NOT_IN (10, 20)
    * // select features with POWER <> 10 and POWER <> 20
@@ -106,9 +106,9 @@ export const EnumOperators = Object.freeze({
  * @example
  * // test if a feature is a 'trafostation'
  * let filter = {
- *  attributeName: 'TYPE',
+ *  field: 'TYPE',
  *  operator: EnumOperators.LIKE,
- *  validValue: 'trafostation'
+ *  value: 'trafostation'
  * }
  * testFeature(feature, filters)
  *
@@ -124,39 +124,39 @@ export const testFeature = (feature, filters) => {
     if (!Array.isArray(filters)) {
       filters = [filters];
     }
-    return filters.every(({ attributeName, validValue, operator = EnumOperators.EQUAL }) => {
-      const currentValue = feature.get(attributeName);
+    return filters.every(({ field, value, operator = EnumOperators.EQUAL }) => {
+      const currentValue = feature.get(field);
       switch (operator) {
         case EnumOperators.BETWEEN:
-          return Number(validValue[0]) <= Number(currentValue) && Number(validValue[1]) >= Number(currentValue);
+          return Number(value[0]) <= Number(currentValue) && Number(value[1]) >= Number(currentValue);
         case EnumOperators.CONTAIN:
-          return currentValue ? currentValue.indexOf(validValue) > -1 : false;
+          return currentValue ? currentValue.indexOf(value) > -1 : false;
         case EnumOperators.EQUAL:
-          return Number(currentValue) === Number(validValue);
+          return Number(currentValue) === Number(value);
         case EnumOperators.GREATER_THAN:
-          return Number(currentValue) > Number(validValue);
+          return Number(currentValue) > Number(value);
         case EnumOperators.GREATER_THAN_OR_EQUAL:
-          return Number(currentValue) >= Number(validValue);
+          return Number(currentValue) >= Number(value);
         case EnumOperators.IN:
           if (!currentValue) return false;
-          const valuesIn = validValue.split(',').map(value => value.toString());
+          const valuesIn = value.split(',').map(value => value.toString());
           return valuesIn.indexOf(currentValue.toString()) > -1;
         case EnumOperators.LESS_THAN:
-          return Number(currentValue) < Number(validValue);
+          return Number(currentValue) < Number(value);
         case EnumOperators.LESS_THAN_OR_EQUAL:
-          return Number(currentValue) <= Number(validValue);
+          return Number(currentValue) <= Number(value);
         case EnumOperators.LIKE:
           if (!currentValue) return false;
-          return currentValue ? currentValue.toString() === validValue.toString() : false;
+          return currentValue ? currentValue.toString() === value.toString() : false;
         case EnumOperators.NOT_EQUAL:
-          return Number(currentValue) !== Number(validValue);
+          return Number(currentValue) !== Number(value);
         case EnumOperators.NOT_IN:
           if (!currentValue) return false;
-          const valuesNotIn = validValue.split(',').map(value => value.toString());
+          const valuesNotIn = value.split(',').map(value => value.toString());
           return valuesNotIn.indexOf(currentValue.toString()) === -1;
         case EnumOperators.NOT_LIKE:
           if (!currentValue) return false;
-          return currentValue ? currentValue.toString() !== validValue.toString() : false;
+          return currentValue ? currentValue.toString() !== value.toString() : false;
       }
     });
   }
